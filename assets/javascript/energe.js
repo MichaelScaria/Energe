4e8fac5c7a2a3b824620a3cdccc9154655fdf5fb
Michael Scaria <michaelscaria@yahoo.com>
2013-11-24 21:06:55 -0600

    added steps

---
+++ assets/javascript/energe.js
@@ -0,0 +1,20 @@
+// function functionname() {
+	// document.getElementById("logo").style.clip="rect(0px, 150px, 145px,145px)";
+// }
+// setTimeout(functionname,5)
+
+// $('#A').on('click', function() {
+
+//     $(".img1").animate({
+//       fontSize: 1
+//     },
+//     {
+//       step: function(now, fx) {
+//           $(this).css('clip', 'rect(0px, '+(now+30)+'px, '+(now+30)+'px, 0px)')
+//       }
+//     }, 1000);
+
+
+// });
+// http://jsfiddle.net/gCGk2/
+
